6f8998c4d35c02f9655098626c304f2947001138
nunera <67439647+nunera@users.noreply.github.com>
2022-12-06 13:01:15 -0600

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,16 +1,10 @@
-<?php
 
-// This is the array where we will store the messages
 $messages = [];
 
-// If the form was submitted, handle the input
 if (isset($_POST['message'])) {
   // Add the message to the array
   $messages[] = $_POST['message'];
 }
 
-// Return the messages as JSON
 header('Content-Type: application/json');
 echo json_encode($messages);
-
-?>
